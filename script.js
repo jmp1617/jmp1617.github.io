e176120c1012f04f02f4f645e3bf36a62a31721a
Jacob <jmp1617@rit.edu>
2017-01-07 18:59:32 -0500

    added project type selector

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
 $(document).ready(function() {
   var $sbody = $('.sbody');
+  var $projects = $('#projects')
   var isColapsed = false;
+  var isColapsedp = false;
   $sbody.hide();
   $('.main').fadeTo(1000,1);
   $('.middle').hide().slideDown(1000);
-  $('.links').hide().slideDown(1400);
-  $('.profile').hide().slideDown(1800);
-  $('.desc').hide().slideDown(2200);
-  $('#name').effect('slide', 2200);
+  $('#name').effect('slide', 1000);
   $('.links').hover(
     function(){
       $('.icon').addClass('iconup');
@@ -48,9 +47,27 @@ $(document).ready(function() {
       $('.dot2').removeClass('iconup');
     }
   );
+  $('#pbody').hover(
+    function(){
+      $('.ptype').addClass('iconhalf');
+    },
+    function(){
+      $('.ptype').removeClass('iconhalf');
+    }
+  );
+  $('.ptype').hover(
+    function(){
+      $(this).removeClass('iconhalf');
+      $(this).addClass('iconup');
+    },
+    function(){
+      $(this).removeClass('iconup');
+      $(this).addClass('iconhalf');
+    }
+  );
   $('.aboutm').click(function(){
     if(isColapsed==false){
-      $sbody.slideDown(2200);
+      $sbody.slideDown(1200);
       $('.sidepanel').fadeTo('slow',1);
       isColapsed = true;
     }
@@ -58,6 +75,16 @@ $(document).ready(function() {
       $sbody.slideUp(200);
       $('.sidepanel').fadeTo(200,0);
       isColapsed = false;
+    }
+  });
+  $('.projb').click(function(){
+    if(isColapsedp==false){
+      $projects.fadeTo('slow',1);
+      isColapsedp = true;
+    }
+    else{
+      $projects.fadeTo(200,0);
+      isColapsedp = false;
     }
   });
   $(document).on('click', '.icon', function(){
@@ -68,5 +95,8 @@ $(document).ready(function() {
   });
   $(document).on('click', '.mailholder', function(){
     $(this).effect('bounce', 400);
+  });
+  $(document).on('click', '.ptype', function(){
+    $(this).effect('highlight', {color:'white'}, 400);
   });
 });
